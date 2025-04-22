e204f8e9cf18545c4348873dda17f194f80e505a
Xinhe Liu <xinhel3@student.unimelb.edu.au>
2025-04-22 13:27:06 +1000

    new twitter demo that extract X data

---
+++ X_API/demo.py
@@ -0,0 +1,27 @@
+import tweepy
+import json
+import os
+
+BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN", "AAAAAAAAAAAAAAAAAAAAAEQZ0wEAAAAAjde%2B77vHE41voFObAtA0v%2FPzHpI%3DzJfGbeQmPsrJMvooz61WWm3RENVjCxf5KMfqfLqXCFpmc6Zrjb")
+
+# Initialize Twitter client using Tweepy
+client = tweepy.Client(bearer_token=BEARER_TOKEN)
+
+# Define search query
+query = "Trump Australia -is:retweet lang:en"
+
+# Request 100 recent tweets
+response = client.search_recent_tweets(
+    query=query,
+    max_results=50,
+    tweet_fields=["id", "text", "created_at", "lang", "author_id"]
+)
+
+# Extract tweet data from response
+tweets = [tweet.data for tweet in response.data or []]
+
+# Write tweets to a JSON file
+with open('twitter_data.json', "w", encoding="utf-8") as f:
+    json.dump(tweets, f, ensure_ascii=False, indent=2)
+
+print(f"{len(tweets)} tweets Saved")

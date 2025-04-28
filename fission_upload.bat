a5c3ea99387b1b12e3729934bac742a4511c27c5
Xinhe Liu <xinhel3@student.unimelb.edu.au>
2025-04-28 18:39:50 +1000

    add pipline bat file that auto update fn and route

---
+++ fission_upload.bat
@@ -0,0 +1,17 @@
+:: This batch script deletes and recreates the 'testyoutube' Fission function and its route.
+
+:: Delete the existing function named 'testyoutube' if it exists
+fission fn delete --name=testyoutube
+
+:: Delete the existing route named 'testyoutube' if it exists
+fission route delete --name=testyoutube
+
+:: Create a new function 'testyoutube' using the source code in Youtube_API/
+:: and attaches the secret 'youtube-api-key'
+fission fn create --name testyoutube --env python39 --src Youtube_API/ --entrypoint youtube_harv.main --secret youtube-api-key
+
+:: Create a new HTTP route 'testyoutube-route' to expose the function
+:: The function will be accessible at '/api/testyoutube' with the GET method
+fission route create --name testyoutube-route --function testyoutube --url /api/testyoutube --method GET
+
+pause
